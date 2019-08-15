86ed7bc16ad2a0fab7c578c6b493e1eb4d2193a4
Affiliatethis <chris@onecrookedtree.com>
2019-08-15 18:27:57 -0400

    added component Images.js

---
+++ src/components/Images.js
@@ -0,0 +1,19 @@
+const images = [
+  {
+    id: 1,
+    src:
+      "https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png",
+    title: "foo",
+    description: "bar"
+  },
+  { id: 2, src: "./assets/image02.jpg", title: "foo", description: "bar" },
+  { id: 3, src: "./assets/image03.jpg", title: "foo", description: "bar" },
+  { id: 4, src: "./assets/image04.jpg", title: "foo", description: "bar" },
+  { id: 5, src: "./assets/image05.jpg", title: "foo", description: "bar" }
+];
+
+function imageLoader() {
+  return images;
+}
+
+export default ImageLoader;
